1fd59f5ae7a176dc90832a3cd7ce9bbda6a87376
KIM.M.J <keyti0729@gmail.com>
2023-10-03 17:36:45 +0900

    feat: PartnerInfo finish design: other_person_profile modify

--- app/src/main/java/com/gmail/sungkyulfriends/MyPage/other_person_profile.java
+++ app/src/main/java/com/gmail/sungkyulfriends/MyPage/other_person_profile.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gmail.sungkyulfriends.MatchingList.matching_list_same_class;
 import com.gmail.sungkyulfriends.R;
+import com.gmail.sungkyulfriends.SeniorAndJunior.GetMatchingPartnerID;
+import com.gmail.sungkyulfriends.SeniorAndJunior.class_matched_firendFirst;
 
 public class other_person_profile extends AppCompatActivity {
 
@@ -24,17 +28,43 @@ public class other_person_profile extends AppCompatActivity {
         back_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(other_person_profile.this, matching_list_same_class.class);
+                Intent intent = new Intent(other_person_profile.this, class_matched_firendFirst.class);
                 startActivity(intent);
             }
         });
 
-        more_review_button = findViewById(R.id.more_review_button);
-        more_review_button.setOnClickListener(new View.OnClickListener() {
+
+        // 상대방 이름
+        TextView partner_name_text = findViewById(R.id.partner_name_text);
+        Intent intent = getIntent();
+        String partner_name = intent.getStringExtra("partner_name");
+        partner_name_text.setText(partner_name);
+
+
+        // 서버에서 상대방정보 가져오기 ////////////////////////////////////////
+        GetMatchingPartnerID getMatchingPartnerID = new GetMatchingPartnerID(this.getApplicationContext());
+        TextView partner_dept_text = findViewById(R.id.partner_dept_text);
+        TextView partner_studentID_text = findViewById(R.id.partner_studentID_text);
+        TextView partner_sex_text = findViewById(R.id.partner_sex_text);
+
+        getMatchingPartnerID.setOnPartnerInfoReceivedListener(new GetMatchingPartnerID.OnPartnerInfoReceivedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(other_person_profile.this, review_page.class);
-                startActivity(intent);
+            public void onPartnerInfoReceived(String partnerID, String name, String sex, String mainDept, String studentID, String deptT) {
+                // 응답을 받아서 처리하는 코드를 여기에 작성합니다.
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        partner_dept_text.setText(mainDept);
+                        Log.d("상대방학과: ", mainDept);
+
+                        partner_studentID_text.setText(studentID);
+                        Log.d("상대방학번: ", studentID);
+
+                        partner_sex_text.setText(sex);
+                        Log.d("상대방성별: ", sex);
+
+                    }
+                });
             }
         });
 
